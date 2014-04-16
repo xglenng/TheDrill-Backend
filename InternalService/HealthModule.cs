6e17bce8363487abba853f0833a4824fa6a5302e
Nicholas Terry <nick.i.terry@gmail.com>
2014-04-15 19:03:54 -0600

    Bootstraped nancy in project and moved some namespaces

---
+++ InternalService/HealthModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nancy;
+
+namespace InternalService
+{
+    public class HealthModule : NancyModule
+    {
+        public HealthModule()
+        {
+            Get["/health"] = x =>
+                {
+                    return new Response { StatusCode = HttpStatusCode.NoContent };
+                };
+        }
+    }
+}
---
+++ TheDrillDaemon/Program.cs
@@ -0,0 +1,20 @@
+﻿using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheDrillDaemon
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            NancyHost host = new NancyHost(new Uri("http://localhost:8080"));
+            host.Start();
+
+            //Loop indefinitely
+            while (true) { };
+        }
+    }
+}
